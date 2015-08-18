678cf81b05d3b790c4f4450cefcb68e48e03285b
Rasmus Reimer <rasmus.reimer@gmail.com>
2015-08-18 03:02:59 +0200

    Get angular-mocks from cdn.

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,6 @@ module.exports = function (config) {
       'https://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.15/angular-ui-router.min.js',
       'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.4/angular-mocks.js',
       'dist/app*.js',
-      'node_modules/angular-mocks/angular-mocks.js',
       'test/*.js'
     ],
     browsers: ['PhantomJS']
